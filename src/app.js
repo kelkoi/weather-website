50d64050944c498ca4b9ecd4c9bddddb3445c230
Khaoula Elkoi <elkokhaoula@gmail.com>
2020-03-11 10:30:33 +0100

    setup app for huroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define pths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -91,6 +92,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is upp on port 3000");
+app.listen(port, () => {
+  console.log("server is upp on port" + port);
 });
